487d3c4c0f53bbcd47d5f5bfa1f9393101a9da0f
tah623 <esmatarsh@gmail.com>
2025-06-29 03:22:58 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "ucAX0KzL#hnFZ-6FSErQme8AqJw89dJ49h1hU8OU9xiLgoSMbCr4", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
 DEV: process.env.DEV || "94779062397",
